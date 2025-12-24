ab0e13b7e0ce1d5565f2bc4438a062c4701c76f5
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2025-12-24 03:31:23 -0800

    Add card animations

--- src/lib/components/MusicCard/MusicCard.svelte
+++ src/lib/components/MusicCard/MusicCard.svelte
@@ -3,33 +3,34 @@
 	import type { Snippet } from 'svelte';
 	import type { PreviewSong } from '../../../routes/music/Preview.svelte';
 
-	interface Props extends PreviewSong {
-		popularitySnippet: Snippet<[popularity: number]>;
+	interface Props {
+		preview: PreviewSong;
+		children: Snippet;
+		dos?: number;
 	}
 
-	let { name, artist, poster, popularity, previewUrl, spotifyUrl, popularitySnippet }: Props =
-		$props();
+	let { preview, children, dos = 1 }: Props = $props();
 </script>
 
 <div class="flex flex-col gap-3 rounded-2xl border-2 p-8 shadow-xl">
-	<a href={spotifyUrl} target="_blank" class="self-center">
+	<a href={preview.spotifyUrl} target="_blank" class="self-center">
 		<img
-			src={poster}
+			src={preview.poster}
 			alt="Song Artwork"
-			class="aspect-square w-60 rounded-2xl border-2 object-cover sm:w-80 shadow-sm"
+			class="aspect-square w-60 rounded-2xl border-2 object-cover shadow-sm sm:w-80"
 		/>
 	</a>
 
 	<div class="max-w-60 sm:max-w-80">
-		<h2 class="line-clamp-2 mb-1 text-lg leading-tight font-bold">{name}</h2>
+		<h2 class=" text-lg leading-5 font-bold">{preview.name.repeat(dos)}</h2>
 		<p class="text-lg">
-			<span class="sr-only">by</span><cite class="line-clamp-2 not-italic">{artist}</cite>
+			<span class="sr-only">by</span><cite class="line-clamp-2 not-italic">{preview.artist}</cite>
 		</p>
 	</div>
 
-	{@render popularitySnippet(popularity)}
+	{@render children()}
 
 	<div class="self-center">
-		<AudioPlayer src={previewUrl} />
+		<AudioPlayer src={preview.previewUrl} />
 	</div>
 </div>
--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -1,61 +1,121 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { highscore } from '../../globalState.svelte';
-	import { Preview } from './Preview.svelte.js';
-	import AudioPlayer from '$lib/components/AudioPlayer.svelte';
+	import { MusicCard } from '$lib/components/MusicCard';
+	import PopularityPlain from '$lib/components/MusicCard/PopularityPlain.svelte';
+	import { Playlist } from './Playlist.svelte';
+	import PopularityGuessable from '$lib/components/MusicCard/PopularityGuessable.svelte';
 
 	const { data } = $props();
+
 	const songs = $derived(data.songs); // Extract the songs array
-	let preview: Preview | null = $state(null);
+
+	const playlist = $derived(new Playlist(songs));
+	onMount(() => {
+		playlist.load();
+	});
 
 	let score = $state(0);
-
 	const incrementScore = () => {
 		score++;
+		highscore.updateScore(score);
 	};
 
-	$effect(() => {
-		preview = new Preview(songs);
-	});
+	let slideCards = $state(false);
+	const onGuess = async () => {
+		await new Promise((r) => setTimeout(r, 2500));
+		slideCards = true;
+
+		await new Promise((r) => setTimeout(r, 1500));
+		slideCards = false;
+		playlist.nextSong();
+	};
 </script>
 
-<div class="flex flex-col items-center justify-center gap-y-4">
-	<div class="justify-self-center">{score}</div>
-	<div>{highscore.value}</div>
+<main
+	class="relative flex flex-col items-center justify-center gap-10 overscroll-none p-8 sm:flex-row sm:p-20"
+>
+	{#if !playlist.loading}
+		{#if playlist.previews[0]}
+			<div data-card1={slideCards}>
+				<MusicCard preview={playlist.previews[0]}>
+					<PopularityPlain popularity={playlist.previews[0].popularity} />
+				</MusicCard>
+			</div>
+		{/if}
 
-	<button class="cursor-pointer rounded-2xl border-2 px-3 py-2" onclick={incrementScore}>
-		test
-	</button>
+		<span class="w-10 text-center text-2xl font-bold">vs</span>
 
-	<button
-		class="cursor-pointer rounded-2xl border-2 px-3 py-2"
-		onclick={() => {
-			highscore.updateScore(score);
-		}}
-	>
-		update Highscore
-	</button>
+		{#if playlist.previews[1]}
+			<div data-card2={slideCards} class="relative">
+				<MusicCard preview={playlist.previews[1]}>
+					{#key playlist.previews[1]}
+						<PopularityGuessable
+							disabled={slideCards}
+							oncorrect={incrementScore}
+							onguessed={onGuess}
+							thisPopularity={playlist.previews[1].popularity}
+							otherPopularity={playlist.previews[1].popularity}
+						/>
+					{/key}
+				</MusicCard>
 
-	{#if preview?.loading}
-		<div>Loading...</div>
-	{:else}
-		<button
-			class="cursor-pointer rounded-2xl border-2 px-3 py-2"
-			onclick={() => preview?.nextSong()}
-		>
-			Get Next song
-		</button>
+				{#if playlist.previews[2]}
+					<div
+						data-card3={slideCards}
+						aria-hidden="true"
+						class="absolute translate-x-[50vw] -translate-y-full"
+					>
+						<MusicCard preview={playlist.previews[2]}>
+							{#key playlist.previews[2]}
+								<PopularityGuessable
+									disabled
+									thisPopularity={playlist.previews[2].popularity}
+									otherPopularity={playlist.previews[2].popularity}
+								/>
+							{/key}
+						</MusicCard>
+					</div>
+				{/if}
+			</div>
+		{/if}
 	{/if}
+</main>
+<button onclick={() => playlist.nextSong()} class="cursor-pointer rounded-2xl border-2 p-2"
+	>Next Song</button
+>
+<button onclick={() => (slideCards = !slideCards)} class="cursor-pointer rounded-2xl border-2 p-2"
+	>Toggle Anim</button
+>
 
-	{#if preview?.firstSong && !preview?.loading}
-		<AudioPlayer src={preview.firstSong?.previewUrl} />
-		{console.log(preview.firstSong?.previewUrl)}
-		{console.log()}
-		<img src={preview.firstSong?.poster} alt="poster" />
-	{/if}
-	{#if preview?.secondSong && !preview?.loading}
-		<AudioPlayer src={preview.secondSong?.previewUrl} />
-		<img src={preview.secondSong?.poster} alt="poster" />
-	{/if}
-	<div>Total Songs: {songs.length}</div>
-</div>
+<style>
+	div[data-card1='true'] {
+		animation: card1 1.5s ease-out forwards;
+	}
+
+	div[data-card2='true'] {
+		animation: card2 1.5s ease-in-out forwards;
+	}
+
+	div[data-card3='true'] {
+		animation: card3 1.5s ease-in-out forwards;
+	}
+
+	@keyframes card1 {
+		to {
+			translate: -50vw;
+		}
+	}
+
+	@keyframes card2 {
+		to {
+			translate: calc(-31.5rem + -4px);
+		}
+	}
+
+	@keyframes card3 {
+		to {
+			transform: translateX(calc(-50vw + 31.5rem + 4px));
+		}
+	}
+</style>
